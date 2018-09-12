cbfc572a0799b7ebf3199c079c99da593c6951ce
kammaii <43202563+kammaii@users.noreply.github.com>
2018-09-12 16:17:26 +0700

    ak_r0

---
+++ js/javascript.js
@@ -0,0 +1,19 @@
+if (!Modernizr.svg) {
+  $("img").attr("src", "images/icon.png");
+}
+
+function btn1() {
+  window.open("consonants.html","_self");
+}
+function btn2() {
+  window.open("vowels.html","_self");
+}
+function btn3() {
+  window.open("lastConsonants.html","_self");
+}
+function btn4() {
+  window.open("assembly.html","_self");
+}
+function backColor() {
+  $("a").css("background-color", "#eff2f7");
+})
