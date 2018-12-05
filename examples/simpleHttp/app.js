e51959bd1bb39aba8b3d1f3096b45b83edf3fb8a
upgradingdave <upgradingdave@gmail.com>
2018-12-05 08:35:52 -0500

    move app.js into it's own file

---
+++ examples/simpleHttp/app.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+  console.log( "ready!" );
+  $('#submitBtn').click(function() {
+    console.log('You clicked!')
+  })
+
+  console.log($('#username').val())
+})
--- examples/simpleHttp/server.js
+++ examples/simpleHttp/server.js
@@ -1,25 +1,44 @@
 // Hyper Text Transport Protocol
 // html = hyper text markup language
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 // common http port : 80
 const port = 3000;
 
+let serveFile = (pathToFile, contentType, httpResponse) => {
+  fs.readFile(pathToFile, function(err, data) {
+    //response.writeHead(200, {'Content-Type': 'text/html'});
+    httpResponse.statusCode = 200;
+    httpResponse.setHeader('Content-Type', contentType);
+    httpResponse.write(data);
+    httpResponse.end();
+  });
+}
+
 let handleRequest = function(httpRequest, httpResponse) {
+  try {
   //console.log(httpResponse);
 
-  httpResponse.statusCode = 200;
+  // what is this request for?
+  // here's where we define the routes
+  console.log(httpRequest.url);
+  if(httpRequest.url == '/app.js'){
+    serveFile('app.js', 'text/javascript', httpResponse);
+  } else {
 
-  // Very simple string response
-  httpResponse.setHeader('Content-Type', 'text/html');
+    httpResponse.statusCode = 200;
 
-  // everything after the headers is the "Response Body"
-  let html = "<html><head><title>Jquery</title></head>" +
-             "<body>"+
-             "<div>Hello Jquery</div>"+
-             "<form method=\"POST\" action=\"/login\">"+
-             "<div>" +
+    // Very simple string response
+    httpResponse.setHeader('Content-Type', 'text/html');
+
+    // everything after the headers is the "Response Body"
+    let html = "<html><head><title>Jquery</title></head>" +
+              "<body>"+
+              "<div>Hello Jquery</div>"+
+              "<form method=\"POST\" action=\"/login\">"+
+              "<div>" +
              "<label for=\"username\"/>Username</label>"+
              "<input id=\"username\" name=\"username\" type=\"text\"></input>" +
              "</div>" +
@@ -27,23 +46,18 @@ let handleRequest = function(httpRequest, httpResponse) {
              "<label for=\"password\"/>Password</label>"+
              "<input name=\"password\" type=\"password\"></input>" +
              "</div>" +
-             "<input id=\"submitBtn\" class=\"btn\" type=\"submit\" value=\"Submit\">" +
+             "<a href=\"#\" id=\"submitBtn\" class=\"btn\" value=\"Submit\">Submit</a>" +
              "</form>"+
              "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js\"></script>"+
-             "<script>" +
-             "$( document ).ready(function() {" +
-             "  console.log( \"ready!\" );"+
-             "  $('#submitBtn').click(function() { " +
-             "    console.log('You clicked!') "+
-             "    console.log($('#username').value) "+
-             "  }) "+
-             "});" +
-             "</script>"
-
-             ;
+             "<script src=\"http://127.0.0.1:3000/app.js\">" +
+             "</script>"+
              "</body>";
 
-  httpResponse.end(html);
+    httpResponse.end(html);
+  }
+} catch(err) {
+  console.log(err);
+}
 
 }
 
