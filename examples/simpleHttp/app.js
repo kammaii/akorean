a5aee5a6240c881489c8205c0e5a8a92f7e7633e
upgradingdave <upgradingdave@gmail.com>
2018-12-06 08:29:06 -0500

    Add basic username validation

--- examples/simpleHttp/app.js
+++ examples/simpleHttp/app.js
@@ -1,8 +1,23 @@
 $( document ).ready(function() {
   console.log( "ready!" );
   $('#submitBtn').click(function() {
+
     console.log('You clicked!')
+
+    let value = $('#username').val()
+
+    console.log(`The current value of the input is '${value}'`)
+
+    if(value != undefined && value != null && value.length > 0) {
+      console.log("Good!")
+      // submit the form
+
+    } else {
+      console.log("BAD!")
+      let infoMsgEl = $('#info-msg')
+      infoMsgEl.html("<p>Oops! Please enter a username to continue!</p>");
+    }
+
   })
 
-  console.log($('#username').val())
 })
