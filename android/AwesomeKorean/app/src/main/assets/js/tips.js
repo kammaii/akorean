abd8939eb41015087bfbfd3f4af973902947a523
upgradingdave <upgradingdave@gmail.com>
2019-02-14 08:35:40 -0500

    basic random tips are working

--- android/AwesomeKorean/app/src/main/assets/js/tips.js
+++ android/AwesomeKorean/app/src/main/assets/js/tips.js
@@ -1,5 +1,35 @@
+var tips = 
+  {
+    "tip0": {
+      "timestamp": "20190212",
+      "message": "Message 1"
+    },
+
+    "tip1": {
+      "timestamp": "20190212",
+      "message": "Message 2"
+    },
+
+    "tip2": {
+      "timestamp": "20190212",
+      "message": "Message 3"
+    }
+  }
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+
 $( document ).ready(function() {
-    // load tips.json
-    // choose a tip (randomly)
-    // display in footer. 
+  // choose a tip (randomly)
+  let tipsLength = Object.keys(tips).length;
+
+  let randomNumber = getRandomInt(tipsLength);
+  console.log(randomNumber);
+
+  
+  let randomTip = tips["tip" + randomNumber];
+  
+  $('.page__footer').html("<p>" + randomTip.message + "</p>");
 });
