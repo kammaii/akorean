7842c461ef6899190d78e5b4e08b72b04f21f1ef
upgradingdave <upgradingdave@gmail.com>
2019-05-30 08:36:53 -0400

    implemented array of notes inside Notes class

--- todo/src/main/java/net/awesomekorean/Notes.java
+++ todo/src/main/java/net/awesomekorean/Notes.java
@@ -3,6 +3,7 @@ package net.awesomekorean;
 public class Notes {
 
   private Note[] todos;
+  private int noteCount = 0;
 
   public Notes() {
     todos = new Note[0];
@@ -14,11 +15,36 @@ public class Notes {
 
   public void addTodo(String newTodo) {
 
-    // Add newTodo to array of todos
-    // How many slots are available in todos?
-    todos = new Note[1];
-    todos[0] = new Note(newTodo);
+    Note[] oldTodos = todos;
 
+    noteCount++;
+    todos = new Note[noteCount];
+
+    // copy old todos into new todos
+    for(int i=0; i<oldTodos.length; i++) {
+      todos[i] = oldTodos[i];
+    }
+
+    todos[noteCount-1] = new Note(newTodo);
+
+  }
+
+  public void removeTodo(int arrayIndex) {
+
+    // create array that is one index less than todos
+    noteCount--;
+    Note[] newTodos = new Note[noteCount];
+
+    // todos should contain all of the original todos except for arrayIndex
+    int j = 0;
+    for(int i=0; i<todos.length; i++) {
+      if(arrayIndex != i) {
+        newTodos[j] = todos[i];
+        j++;
+      }
+    }
+
+    todos = newTodos;
 
   }
 }
--- todo/src/test/java/net/awesomekorean/NoteTest.java
+++ todo/src/test/java/net/awesomekorean/NoteTest.java
@@ -34,6 +34,35 @@ public class NoteTest {
 
     assertTrue(notes.getTodos().length ==  2);
 
+    assertEquals("Take Kids to School.", notes.getTodos()[1].getTodo());
+    assertEquals("Buy food from store.", notes.getTodos()[0].getTodo());
+
+  }
+
+
+  @Test
+  public void testRemoveNotes() {
+
+    Notes notes = new Notes();
+    assertTrue(notes.getTodos().length ==  0);
+
+    notes.addTodo("Buy food from store.");
+
+    assertTrue(notes.getTodos().length ==  1);
+
+    notes.addTodo("Take Kids to School.");
+
+    assertTrue(notes.getTodos().length ==  2);
+
+    assertEquals("Buy food from store.", notes.getTodos()[0].getTodo());
+    assertEquals("Take Kids to School.", notes.getTodos()[1].getTodo());
+
+    notes.removeTodo(0);
+
+    assertTrue(notes.getTodos().length == 1);
+
+    assertEquals("Take Kids to School.", notes.getTodos()[0].getTodo());
+
   }
 
 }
